5c7877781441273654afa1d79738a42c5694f636
Sid <siddharthpillai123@gmail.com>
2025-07-03 23:40:12 -0700

    format

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,23 +4,20 @@ import { Hero, HeroSubtitle, HeroTitle } from "@/components/hero";
 export default function Home() {
   return (
     <div>
-        <Container>
-          <Hero>
-            <HeroTitle>
-              Linear is a better way
-              <br />
-              to build products
-            </HeroTitle>
-            <HeroSubtitle>
-              Meet the new standard for modern software development. <br />
-              Streamline issues, sprints and product roadmaps.
-            </HeroSubtitle>
-            <img src="/linear.webp" alt="Linear" />
-
-          </Hero>
-        </Container>
-
-    
+      <Container>
+        <Hero>
+          <HeroTitle>
+            Linear is a better way
+            <br />
+            to build products
+          </HeroTitle>
+          <HeroSubtitle>
+            Meet the new standard for modern software development. <br />
+            Streamline issues, sprints and product roadmaps.
+          </HeroSubtitle>
+          <img src="/linear.webp" alt="Linear" />
+        </Hero>
+      </Container>
     </div>
   );
 }
